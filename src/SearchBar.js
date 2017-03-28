2eadbce3e145c4b249d3ad06f05333c9b37c0bed
Žilvinas Urbonas <zilvinas.urbon@gmail.com>
2017-03-28 23:54:17 +0300

    search-bar: do not prefill text in input field

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -3,8 +3,7 @@ import React, { Component } from 'react';
 class SearchBar extends Component {
   constructor(props) {
     super(props);
-
-    this.state = { term: 'runnin' };
+    this.state = { term: '' };
   }
 
   render() {
