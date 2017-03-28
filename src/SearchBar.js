d981a96fe404055c3632e90e76a52cc221606a4c
Žilvinas Urbonas <zilvinas.urbon@gmail.com>
2017-03-28 23:54:37 +0300

    search-bar: change sizing of input field for search

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -9,11 +9,12 @@ class SearchBar extends Component {
   render() {
     return (
       <div className="ant-row search-bar">
-        <div className="ant-col-18 ant-col-offset-3">
+        <div className="ant-col-6 ant-col-offset-9">
           <input
             className="ant-input ant-input-lg"
-            value={this.state.term}
-            onChange={event => this.onInputChange(event.target.value)} />
+            onChange={event => this.onInputChange(event.target.value)}
+            placeholder="Enter the video name"
+            value={this.state.term} />
         </div>
       </div>
     )
